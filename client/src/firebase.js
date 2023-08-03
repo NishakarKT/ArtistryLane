a560049f57a0977b50021716bfe3c43cdd1b511c
NishakarKT <nishakarkumar0@outlook.com>
2023-08-03 09:52:10 +0530

    updated firebase config

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBP3ya7CCJxJHbwwwx76DoHrOGcCJ-9c8k",
-    authDomain: "quikmartt.firebaseapp.com",
-    projectId: "quikmartt",
-    storageBucket: "quikmartt.appspot.com",
-    messagingSenderId: "194662547496",
-    appId: "1:194662547496:web:e5f89ca532bf7c543fb51d",
-    measurementId: "G-QB47G4FW58"
+    apiKey: "AIzaSyBYHfGpl0DjGHI7Fhwu5zidYMSTiSWS_-k",
+  authDomain: "artistry-lane.firebaseapp.com",
+  projectId: "artistry-lane",
+  storageBucket: "artistry-lane.appspot.com",
+  messagingSenderId: "914350550285",
+  appId: "1:914350550285:web:dbe20a683b864653c59444",
+  measurementId: "G-Y77PRRHMHZ"
 };
 
 firebase.initializeApp(firebaseConfig);
